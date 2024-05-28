6de38f1229006e293ae6c61eb9ea7a62916e57b6
AlessandraRivasplata <162304095+AlessandraRivasplata@users.noreply.github.com>
2024-05-28 22:05:23 +0200

    holix2

--- src/dao/DaoImplJDBC.java
+++ src/dao/DaoImplJDBC.java
@@ -9,29 +9,37 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DaoImplJDBC implements Dao {
-    private Connection connection;
+    private Connection connection; // OBJETO CONNECTION PARA GESTIONAR LA CONEXIÓNA A NUESTRA BASE DE DATOS
 
     @Override
-    public void connect() {
+    public void connect()  {
         try {
+            // Define connection parameters
+            String url = "jdbc:mysql://localhost:8889/shop";
+            String user = "root";
+            String pass = "root";
+            
             Class.forName("com.mysql.cj.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/shop", "root", "root");
+            this.connection = DriverManager.getConnection(url, user, pass);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
+           
         }
     }
 
-    @Override
+
+    @Override // MÉTODO PARA OBTENER UN EMPLEADO POR SU ID Y CONTRASEÑA
     public Employee getEmployee(int employeeId, String password) {
         Employee employee = null;
         try {
+        	// CONSULTA SQL PARA SELECIONAR EMPPELADO POR SU ID Y CONTRASEÑA
             String query = "SELECT * FROM employee WHERE employeeId = ? AND password = ?";
             PreparedStatement stmt = connection.prepareStatement(query);
-            stmt.setInt(1, employeeId);
+            stmt.setInt(1, employeeId); // ESTABLECEMOS PARAMETROS EN LA CONSULTA
             stmt.setString(2, password);
-            ResultSet rs = stmt.executeQuery();
+            ResultSet rs = stmt.executeQuery(); // EJECUTAMOS LA CONSULTA PARA TENER EL RESULTADO
 
-            if (rs.next()) {
+            if (rs.next()) {// SI LO HAY SE CREAR UN OBJETO EMPLOYEE
                 employee = new Employee();
                 employee.setEmployeeId(rs.getInt("employeeId"));
                 employee.setName(rs.getString("name"));
@@ -44,7 +52,7 @@ public class DaoImplJDBC implements Dao {
     }
 
     @Override
-    public void disconnect() {
+    public void disconnect() { // METODO PARA DESCONECTAR LA BASE DE DATOS 
         try {
             if (connection != null && !connection.isClosed()) {
                 connection.close();
